6b6293e0890ea6860bec8e92c67288b785bfa2ac
mrutyunjaya <mrmjpatra@gmail.com>
2022-01-25 22:16:29 +0530

    program1 has been changed

--- Section1/program _1/program1.c
+++ Section1/program _1/program1.c
@@ -1,23 +1,27 @@
+/*Write an algorithm and draw the flow chart to compute the smallest of three numbers.*/
 #include<stdio.h>
 #include<conio.h>
+
+
 void main(){
-    int a,b,c,largest;
-    printf("Enter 3 numbers : \n");
+    int a,b,c,smallest;
+    printf("Enter 3 numbers : ");
+    printf("\n------------------------------\n");
+
     printf("Enter 1st number : ");
     scanf("%d",&a);
     printf("Enter 2nd number : ");
     scanf("%d",&b);
     printf("Enter 3rd number : ");
     scanf("%d",&c);
-    largest = a;
+    smallest = a;
 
-    if (b > largest){
-        largest = b;
+    if (b<smallest){
+        smallest= b;
     }
-    else if (c > largest){
-        largest = c;
+    else if (c<smallest){
+        smallest= c;
     }
 
-    printf("Largest Number is : %d",largest);
-    
+    printf("Largest Number is : %d",smallest);
 }
