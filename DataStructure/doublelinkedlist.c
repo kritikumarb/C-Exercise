6706e7c1af36b0ca53790513cd3329a54301a690
kritikumar <kritikumarb@gmail.com>
2022-03-10 11:08:48 +0530

    Optimised Double Linked List

--- DataStructure/doublelinkedlist.c
+++ DataStructure/doublelinkedlist.c
@@ -12,6 +12,9 @@ void insertElementEnd(struct node *hea , int x);
 snode* insertElementStart(struct node *hea , int x);
 void insertAtAnyPoint(snode *hea , int x);
 void showList(struct node *hea);
+snode* deleteFirst(struct node *hea);
+void deleteLast(snode *hea);
+void deleteAny(snode *hea , int x);
 void main()
 {
     int c=1,k,n;
@@ -20,39 +23,81 @@ void main()
     printf("Enter first node next : ");
     scanf("%d",&head->info);
     head->next = NULL;
-     while(c){
-    printf("\n\nEnter Your Choice : \n");
-    printf("1)Show All Elements  \n");
-    printf("2)Insert At Start  \n");
-    printf("3)Insert at End  \n");
-    printf("4)Insert At Any Point\n");
-    printf("5)End Of Loop \n");
-    scanf("%d",&k);
-    switch(k){
-        case 1:
-            showList(head);
-            break;
-        case 2:
-            printf("Enter Number(END) : ");
-            scanf("%d",&n);
-            head = insertElementStart(head , n);
-            break;
-        case 3:
-            printf("Enter Number(END) : ");
-            scanf("%d",&n);
-            insertElementEnd(head , n);
-            break;
-        case 4:
-            printf("Enter The previous location of Insertion : ");
-            scanf("%d",&n);
-            insertAtAnyPoint(head , n);
-            break;
-        default:
-            free(head);
-            exit(0);
+    while(c){
+        printf("\n\nEnter Your Choice : \n");
+        printf("1)Show All Elements  \n");
+        printf("2)Insert At Start  \n");
+        printf("3)Insert at End  \n");
+        printf("4)Insert At Any Point\n");
+        printf("5)Delete The First Node\n");
+        printf("6)Delete The Last Node\n");
+        printf("7)Delete An Intermediate Node\n");
+        printf("8)End Of Loop \n");
+        scanf("%d",&k);
+        switch(k){
+            case 1:
+                showList(head);
+                break;
+            case 2:
+                printf("Enter Number(END) : ");
+                scanf("%d",&n);
+                head = insertElementStart(head , n);
+                break;
+            case 3:
+                printf("Enter Number(END) : ");
+                scanf("%d",&n);
+                insertElementEnd(head , n);
+                break;
+            case 4:
+                printf("Enter The previous location of Insertion : ");
+                scanf("%d",&n);
+                insertAtAnyPoint(head , n);
+                break;
+            case 5:
+                printf("\nDeleting The First Node...\n");
+                head = deleteFirst(head);
+                break;
+            case 6:
+                printf("\nDeleting The Last Node...\n");
+                deleteLast(head);
+                break;
+            case 7:
+                printf("Enter the node To delete : ");
+                scanf("%d",&n);
+                deleteAny(head , n);
+                printf("\n Deleting The Node : ");
+                break;
+            default:
+                free(head);
+                exit(0);
+            }
         }
-    }
     getch();
+}
+
+void deleteAny(snode *hea , int x){
+
+    snode *ptr1= hea;
+    snode *ptr2= ptr1->next;
+    snode *ptr3= ptr2->next;
+    if(ptr1->info == x){
+        deleteFirst(hea);
+        return;
+    }
+    while(ptr3->next!=NULL && ptr2->info!=x){
+        ptr1 = ptr1->next;
+        ptr2 = ptr2->next;
+        ptr3 = ptr3->next;
+    }
+
+    if(ptr3->info == x){
+        deleteLast(hea);
+    }
+    if(ptr2->info == x){
+        ptr1->next = ptr3;
+        ptr3->prev = ptr1;
+        free(ptr2);
+    }
 }
 void insertElementEnd(struct node *hea , int x){
     struct node *ptr1 = hea;
@@ -64,6 +109,33 @@ void insertElementEnd(struct node *hea , int x){
     ptr1->next = current;
     current->prev = ptr1;
     current->next = NULL;
+}
+void deleteLast(snode *hea){
+    if(hea->next==NULL){
+        free(hea);
+        main();
+        return;
+    }
+    snode *ptr = hea;
+    snode *ptr2 = hea->next;
+    while(ptr2->next!=NULL){
+        ptr = ptr->next;
+        ptr2= ptr2->next;
+    }
+    ptr->next = NULL;
+    free(ptr2);
+
+
+}
+snode* deleteFirst(struct node *hea){
+    if(hea->next == NULL){
+        main();
+        return;
+    }
+    snode *ptr = hea;
+    ptr->next->prev = NULL;
+    ptr = ptr->next;
+    return ptr;
 }
 
 snode* insertElementStart(struct node *hea , int x){
